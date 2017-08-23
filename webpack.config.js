f9109c51b30d5522ecccea004876b4317581f70b
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-08-23 12:58:34 +0800

    added html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,21 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
     entry: './src/app.js',
     output: {
-        filename: './dist/app.bundle.js'
-    }
+        path: __dirname + '/dist',
+        filename: 'app.bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Markdown Previewer',
+            /*
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            */
+            template: './src/index.ejs', // Load a custom template (ejs by default see the FAQ for details)
+        })
+    ]
 }
