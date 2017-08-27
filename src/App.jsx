a3c0d03afd4c5c446ad16826d3438abba4155a42
Greg Wong <Gregory.LT.Wong@gmail.com>
2017-08-27 17:36:18 +0800

    Set up general layout of page

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,26 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <h1>Hello there!</h1>
+        <header className="text-center">
+          <h1>Markdown Previewer</h1>
+          <div className="m-2">
+            <p>Type your markdown text on the left, and see it converted with formatting applied on the right!</p>
+          </div>
         </header>
+        <div className="container-fluid h-100">
+
+          <div className="row">
+            <div className="col-6">
+              <textarea className="form-control w-100" name="" id="" rows="20" placeholder="Enter markdown text here">
+                Todo: add default text
+                Todo: set the font of the text-area to courier
+              </textarea>
+            </div>
+            <div className="col-6">
+              Todo
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
